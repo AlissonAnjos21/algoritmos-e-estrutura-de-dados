ecb356f4e864f6b7337f2c2e251dcaa252eb6dea
Alisson Anjos <alissondosanjos@gmail.com>
2024-04-04 17:35:12 -0300

    Criando atributo tamanho

--- src/ligadasimplesrecomendada/ListaLigadaSimples.java
+++ src/ligadasimplesrecomendada/ListaLigadaSimples.java
@@ -2,16 +2,23 @@ package ligadasimplesrecomendada;
 
 public class ListaLigadaSimples<T> {
     private No<T> inicio;
+    private int tamanho = 0;
 
     public void adicionar(T elemento){
         No<T> no = new No<>(elemento);
         this.inicio = no;
+        this.tamanho++;
+    }
+
+    public int getTamanho(){
+        return this.tamanho;
     }
 
     @Override
     public String toString() {
         return "ListaLigadaSimples{" +
                 "inicio=" + inicio +
+                ", tamanho=" + tamanho +
                 '}';
     }
 }
