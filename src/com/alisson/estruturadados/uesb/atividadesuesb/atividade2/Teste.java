6364737f7907da40c41bd9131636c8bef1bdd7c1
Alisson Anjos <93294707+AlissonAnjos21@users.noreply.github.com>
2024-04-06 21:34:28 -0300

    Teste

--- src/com/alisson/estruturadados/uesb/atividadesuesb/atividade2/Teste.java
+++ src/com/alisson/estruturadados/uesb/atividadesuesb/atividade2/Teste.java
@@ -2,6 +2,15 @@ package com.alisson.estruturadados.uesb.atividadesuesb.atividade2;
 
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("Oi");
+        ListaEncadeada lista = new ListaEncadeada();
+
+        lista.inserirFim(21);
+        lista.inserirInicio(42);
+        lista.inserirFim(63);
+        lista.inserirInicio(84);
+        // 84 42 21 63
+
+        System.out.println(lista);
+
     }
 }
