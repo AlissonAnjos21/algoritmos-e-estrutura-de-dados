333827161657925eac6e4a20e5c3221d20ea20a3
Alisson Anjos <93294707+AlissonAnjos21@users.noreply.github.com>
2024-04-14 23:53:10 -0300

    Testes

--- src/com/alisson/estruturadados/uesb/atividadesuesb/atividade5/Teste.java
+++ src/com/alisson/estruturadados/uesb/atividadesuesb/atividade5/Teste.java
@@ -1,5 +1,26 @@
 package com.alisson.estruturadados.uesb.atividadesuesb.atividade5;
 
 public class Teste {
+    public static void main(String[] args) throws Exception{
 
+        FilaArray<Integer> filaArray = new FilaArray<>(3);
+        System.out.println(filaArray.estaVazia());
+
+        filaArray.enfileirar(1);
+        System.out.println(filaArray);
+
+        filaArray.enfileirar(2);
+        filaArray.enfileirar(3);
+        System.out.println(filaArray);
+
+        System.out.println("Desenfileirando...");
+        System.out.println(filaArray.desenfileirar());
+        System.out.println(filaArray);
+
+        System.out.println(filaArray.getPrimeiro());
+
+        filaArray.fazVazia();
+        System.out.println(filaArray);
+
+    }
 }
